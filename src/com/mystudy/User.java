a46f82b9db04825316c0ccc309f863f50a123d1e
wanshiruyi <wanshiruyi.com>
2022-07-16 22:59:24 +0800

    111-commit

---
+++ src/com/mystudy/User.java
@@ -0,0 +1,11 @@
+package com.mystudy;
+
+/**
+ * @author :dingyb
+ * @date : 2022/7/16 22:57
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
