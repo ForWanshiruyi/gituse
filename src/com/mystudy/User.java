d608dd472c4050db22770bf86817ecbd085e5370
wanshiruyi <wanshiruyi.com>
2022-07-16 23:11:10 +0800

    333-commit

--- src/com/mystudy/User.java
+++ src/com/mystudy/User.java
@@ -8,5 +8,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("222");
+        System.out.println("333");
     }
 }
